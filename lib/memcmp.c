4b52df0ec1cc0d7bfb225bd043c741fc1d2f5f2a
Reuben Thomas <rrt@sc3d.org>
2003-05-25 20:46:40 +0000

    Format source in linux style.

--- lib/memcmp.c
+++ lib/memcmp.c
@@ -18,12 +18,12 @@
  */
 int memcmp(const void *s1, const void *s2, size_t n)
 {
-  if (n) {
-    const unsigned char *p1 = s1, *p2 = s2;
-    do {
-      if (*p1++ != *p2++)
-        return (*--p1 - *--p2);
-    } while (--n);
-  }
-  return 0;
+        if (n) {
+                const unsigned char *p1 = s1, *p2 = s2;
+                do {
+                        if (*p1++ != *p2++)
+                                return (*--p1 - *--p2);
+                } while (--n);
+        }
+        return 0;
 }
