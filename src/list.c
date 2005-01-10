aa41700ab2380a535ef359643db42bc3d04f9bac
Reuben Thomas <rrt@sc3d.org>
2005-01-10 00:24:03 +0000

    Add license to top of file.

--- src/list.c
+++ src/list.c
@@ -1,4 +1,25 @@
-/* Circular doubly-linked lists/queues */
+/* Circular doubly-linked lists/queues
+   Copyright (c) 1997-2005 Reuben Thomas.
+   All rights reserved.
+
+   This file is part of Zile.
+
+   Zile is free software; you can redistribute it and/or modify it under
+   the terms of the GNU General Public License as published by the Free
+   Software Foundation; either version 2, or (at your option) any later
+   version.
+
+   Zile is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or
+   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+   for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Zile; see the file COPYING.  If not, write to the Free
+   Software Foundation, 59 Temple Place - Suite 330, Boston, MA
+   02111-1307, USA.  */
+
+/*	$Id: list.c,v 1.2 2005/01/10 00:24:03 rrt Exp $	*/
 
 #include <stdlib.h>
 #include <assert.h>
@@ -6,7 +27,6 @@
 #include "list.h"
 #include "zile.h"
 #include "extern.h"
-
 
 /* Create an empty list, returning a pointer to the list */
 list list_new(void)
