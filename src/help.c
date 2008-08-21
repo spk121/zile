0f416aa529232cdab3f1bccfd5c5a0cbc70b38c3
Reuben Thomas <rrt@sc3d.org>
2008-08-21 22:53:33 +0000

    Improve description of zile-version.

--- src/help.c
+++ src/help.c
@@ -37,7 +37,7 @@
 
 DEFUN ("zile-version", zile_version)
 /*+
-Show the zile version.
+Show the version of Zile that is running.
 +*/
 {
   minibuf_write (ZILE_VERSION_STRING " of " CONFIGURE_DATE " on " CONFIGURE_HOST);
