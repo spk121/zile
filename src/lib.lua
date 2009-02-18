d7d7f4a83364e66d613c8de3c2f07451a0bf1396
Reuben Thomas <rrt@sc3d.org>
2009-02-18 21:58:13 +0000

    Remove require function, as it doesn't search the path, whereas the official function does raise an error if the file is not found, which is exactly what we want. Not sure why we replaced the function in Zee.

---
+++ src/lib.lua
@@ -0,0 +1,44 @@
+-- Zee-specific library functions
+--
+-- Copyright (c) 2009 Free Software Foundation, Inc.
+-- Copyright (c) 2006, 2007, 2008 Reuben Thomas.
+--
+-- This file is part of GNU Zile.
+--
+-- GNU Zile is free software; you can redistribute it and/or modify it
+-- under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 3, or (at your option)
+-- any later version.
+--
+-- GNU Zile is distributed in the hope that it will be useful, but
+-- WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+-- General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with GNU Zile; see the file COPYING.  If not, write to the
+-- Free Software Foundation, Fifth Floor, 51 Franklin Street, Boston,
+-- MA 02111-1301, USA.
+
+
+-- Parse re-usable C headers
+function X (...)
+  xarg = {...}
+end
+
+-- Extract the docstrings from tbl_funcs.lua
+docstring = {}
+function def (t)
+  local name, s
+  for i, v in pairs (t) do
+    if type (i) == "string" then
+      name = i
+      _G[name] = v
+    elseif type (i) == "number" and i == 1 then
+      s = v
+    end
+  end
+  if name then
+    docstring[name] = s
+  end
+end
