26ca03d7ba89566be0c5a4977a681fe9696b8744
Reuben Thomas <rrt@sc3d.org>
2009-02-19 15:24:38 +0000

    Generate dotzile.texi in mkvars.lua.
    Move some parts of Makefile.am around for legibility.

--- src/mkvars.lua
+++ src/mkvars.lua
@@ -34,10 +34,17 @@ assert (h1)
 h2 = io.open ("dotzile.sample", "w")
 assert (h2)
 
+h3 = io.open ("dotzile.texi", "w")
+assert (h3)
+
 h1:write ("@c Automatically generated file: DO NOT EDIT!\n")
 h1:write ("@table @code\n")
 
 h2:write ("; ." .. PACKAGE .. " configuration\n\n")
+
+h3:write ("@c Automatically generated file: DO NOT EDIT!\n")
+h3:write ("@setfilename dotzile.info\n")
+h3:write ("@example\n")
 
 local vars = {}
 for l in io.lines (arg[1]) do
@@ -78,5 +85,7 @@ h1:close ()
 
 -- Add extra key rebindings to sample configuration file
 h2:write ("; Rebind keys\n; (global-set-key \"key\" 'func)\n\n; Better bindings for when backspace generates C-h\n;(global-set-key \"\\BACKSPACE\"  'backward-delete-char)\n;(global-set-key \"\\C-h\"        'backward-delete-char)\n;(global-set-key \"\\M-:\"        'mark-paragraph)\n;(global-set-key \"\\M-hb\"       'list-bindings)\n;(global-set-key \"\\M-hd\"       'describe-function)\n;(global-set-key \"\\M-hf\"       'describe-function)\n;(global-set-key \"\\M-hF\"       'view-zile-FAQ)\n;(global-set-key \"\\M-hk\"       'describe-key)\n;(global-set-key \"\\M-hlr\"      'list-registers)\n;(global-set-key \"\\M-hs\"       'help-config-sample)\n;(global-set-key \"\\M-ht\"       'help-with-tutorial)\n;(global-set-key \"\\M-hw\"       'where-is)\n;(global-set-key \"\\M-hv\"       'describe-variable)\n")
-
 h2:close ()
+
+h3:write ("@end example\n")
+h3:close ()
