d619f7eb10198f3d1ced094914a12c0e90259860
Reuben Thomas <rrt@sc3d.org>
2009-04-07 00:19:20 +0100

    Process tbl_vars.h with mkvars.lua, to process formatting directives.

--- src/mkvars.lua
+++ src/mkvars.lua
@@ -26,24 +26,39 @@ prog = {
 require "lib"
 require "texinfo"
 
-h = io.open ("dotzile.sample", "w")
-assert (h)
+h1 = io.open ("tbl_vars.h", "w")
+assert (h1)
 
-h:write ("; ." .. PACKAGE .. " configuration\n\n")
+h2 = io.open ("dotzile.sample", "w")
+assert (h2)
+
+h2:write ("; ." .. PACKAGE .. " configuration\n\n")
 -- Don't note where the contents of this file comes from or that it's
 -- auto-generated, because it's ugly in a user configuration file.
+
+function escape_for_C (s)
+  s = string.gsub (s, "\n", "\\n")
+  s = string.gsub (s, "\"", "\\\"")
+  return s
+end
 
 local vars = {}
 for l in io.lines (arg[1]) do
   if string.find (l, "^X %(") then
     assert (loadstring (l)) ()
     local name, defval, local_when_set, doc = unpack (xarg)
+    doc = texi (doc)
 
-    h:write ("; " .. string.gsub (texi (doc), "(\n", "\n; ") .. "\n")
-    h:write ("; Default value is " .. defval .. ".\n")
-    h:write ("(setq " .. name .. " " .. defval .. ")\n")
-    h:write ("\n")
+    h1:write ("X (\"" .. name .. "\", \"" .. defval .. "\", " ..
+              tostring (local_when_set) .. ", \"" ..
+              escape_for_C (doc) .. "\")\n")
+
+    h2:write ("; " .. string.gsub (doc, "(\n", "\n; ") .. "\n")
+    h2:write ("; Default value is " .. defval .. ".\n")
+    h2:write ("(setq " .. name .. " " .. defval .. ")\n")
+    h2:write ("\n")
   end
 end
 
-h:close ()
+h1:close ()
+h2:close ()
