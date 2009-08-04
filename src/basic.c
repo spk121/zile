313ba93b5a47a4b08f1f6ad9617ad332dbbe637d
Reuben Thomas <rrt@sc3d.org>
2009-08-04 22:10:56 +0200

    Change conditions under which scroll-{up,down} return an error, to match Emacs.

--- src/basic.c
+++ src/basic.c
@@ -360,7 +360,7 @@ END_DEFUN
 static bool
 scroll_down (void)
 {
-  if (get_buffer_pt (cur_bp).n > 0)
+  if (get_buffer_pt (cur_bp).n > get_window_eheight (cur_wp))
     return move_line (-get_window_eheight (cur_wp));
 
   minibuf_error ("Beginning of buffer");
@@ -370,7 +370,8 @@ scroll_down (void)
 static bool
 scroll_up (void)
 {
-  if (get_buffer_pt (cur_bp).n < get_buffer_last_line (cur_bp))
+  if (get_buffer_pt (cur_bp).n <
+      get_buffer_last_line (cur_bp) - get_window_eheight (cur_wp))
     return move_line (get_window_eheight (cur_wp));
 
   minibuf_error ("End of buffer");
