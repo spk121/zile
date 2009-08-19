5480b18f1c754630e6d07b9acaab51101074a9b6
Reuben Thomas <rrt@sc3d.org>
2009-08-19 23:02:22 +0100

    Fix scrolling of completions after recent changes in scrolling.

--- src/completion.c
+++ src/completion.c
@@ -125,7 +125,7 @@ completion_scroll_up (void)
   assert (wp != NULL);
   set_current_window (wp);
   pt = get_buffer_pt (cur_bp);
-  if (pt.n == get_buffer_last_line (cur_bp) || !FUNCALL (scroll_up))
+  if (pt.n >= get_buffer_last_line (cur_bp) - get_window_eheight (cur_wp) || !FUNCALL (scroll_up))
     gotobob ();
   set_current_window (old_wp);
 
