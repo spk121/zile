a3aac30d1d915eb819d223b1f67f85ae7af7242d
Reuben Thomas <rrt@sc3d.org>
2011-03-03 14:42:20 +0000

    Modify isearch_forward_regexp test to catch just-fixed bug.

--- src/lisp-tests/isearch_forward_regexp.el
+++ src/lisp-tests/isearch_forward_regexp.el
@@ -1,2 +1,2 @@
-; isearch-forward-regexp a . p RET f save-buffer save-buffers-kill-emacs
-(execute-kbd-macro "\C-\M-sa.p\rf\C-x\C-s\C-x\C-c")
+; isearch-forward-regexp  [ a ] . p RET f save-buffer save-buffers-kill-emacs
+(execute-kbd-macro "\C-\M-s[a].p\rf\C-x\C-s\C-x\C-c")
