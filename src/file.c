f7af713330fb768f490d6fc404ed48474d30c273
Reuben Thomas <rrt@sc3d.org>
2011-03-13 09:08:00 +0000

    Remove useless include of assert.

--- src/file.c
+++ src/file.c
@@ -1,6 +1,6 @@
 /* Disk file handling
 
-   Copyright (c) 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009 Free Software Foundation, Inc.
+   Copyright (c) 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011 Free Software Foundation, Inc.
 
    This file is part of GNU Zile.
 
@@ -22,7 +22,6 @@
 #include "config.h"
 
 #include <sys/stat.h>
-#include <assert.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <limits.h>
