9b8990e4dd74b0c6c48df5bde92f65e51be3aadb
Reuben Thomas <rrt@sc3d.org>
2011-06-16 12:36:51 +0100

    Fix a copyright range.

--- src/buffer.c
+++ src/buffer.c
@@ -1,6 +1,6 @@
 /* Buffer-oriented functions
 
-   Copyright (c) 1997-2006, 2008-2010, 2011 Free Software Foundation, Inc.
+   Copyright (c) 1997-2006, 2008-2011 Free Software Foundation, Inc.
 
    This file is part of GNU Zile.
 
