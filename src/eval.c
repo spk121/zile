1b055c3e9bbcea9eaa1bfefeed582a212ec54e70
Reuben Thomas <rrt@sc3d.org>
2011-06-16 12:35:04 +0100

    Fix some copyright ranges.

--- src/eval.c
+++ src/eval.c
@@ -1,6 +1,6 @@
 /* Lisp eval
 
-   Copyright (c) 2001, 2005, 2006, 2007, 2008, 2009, 2010, 2011 Free Software Foundation, Inc.
+   Copyright (c) 2001, 2005-2011 Free Software Foundation, Inc.
 
    This file is part of GNU Zile.
 
--- src/lisp.c
+++ src/lisp.c
@@ -1,6 +1,6 @@
 /* Lisp parser
 
-   Copyright (c) 2001, 2005, 2008, 2009, 2010, 2011 Free Software Foundation, Inc.
+   Copyright (c) 2001, 2005, 2008-2011 Free Software Foundation, Inc.
 
    This file is part of GNU Zile.
 
