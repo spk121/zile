spk121/zile

commit bb28e0734c8fdbd9f6798f1b2275415f78a8dd33

Various bits of simplification and tidy-up.

src/completion.c
@@ -148,13 +148,8 @@ static size_t
 calculate_max_length (gl_list_t l)
 {
   size_t maxlen = 0;
-
   for (size_t i = 0; i < gl_list_size (l); i++)
-    {
-      size_t len = strlen ((const char *) gl_list_get_at (l, i));
-      maxlen = MAX (len, maxlen);
-    }
-
+    maxlen = MAX (strlen ((const char *) gl_list_get_at (l, i)), maxlen);
   return maxlen;
 }
 
@@ -162,8 +157,9 @@ calculate_max_length (gl_list_t l)
  * Print the list of completions in a set of columns.
  */
 static void
-completion_print (gl_list_t l)
+write_completion (va_list ap)
 {
+  gl_list_t l = va_arg (ap, gl_list_t);
   size_t max = calculate_max_length (l) + 5;
   size_t numcols = (get_window_ewidth (cur_wp) - 1) / max;
 
@@ -176,12 +172,6 @@ completion_print (gl_list_t l)
       if (col == 0)
         insert_newline ();
     }
-}
-
-static void
-write_completion (va_list ap)
-{
-  completion_print (va_arg (ap, gl_list_t));
 }
 
 /*
@@ -206,69 +196,47 @@ popup_completion (Completion * cp, int allflag)
  * Reread directory for completions.
  */
 static int
-completion_readdir (Completion * cp, astr as)
+completion_readdir (Completion * cp, astr path)
 {
-  DIR *dir;
-  char *s1, *s2;
-  const char *pdir;
-  char *base;
-  struct dirent *d;
-  struct stat st;
-  astr bs;
-
   cp->completions = gl_list_create_empty (GL_LINKED_LIST,
                                           completion_STREQ, NULL,
                                           NULL, false);
 
-  if (!expand_path (as))
+  if (!expand_path (path))
     return false;
 
-  bs = astr_new ();
-
   /* Split up path with dirname and basename, unless it ends in `/',
-     in which case it's considered to be entirely dirname */
-  s1 = xstrdup (astr_cstr (as));
-  s2 = xstrdup (astr_cstr (as));
-  if (astr_get (as, astr_len (as) - 1) != '/')
+     in which case it's considered to be entirely dirname. */
+  astr pdir, base;
+  if (astr_get (path, astr_len (path) - 1) != '/')
     {
-      char *s = dir_name (s1);
-      /* Append `/' to dir */
-      astr_cat_cstr (bs, s);
-      if (astr_get (bs, astr_len (bs) - 1) != '/')
-        astr_cat_char (bs, '/');
-      pdir = astr_cstr (bs);
-      base = base_name (s2);
+      pdir = astr_new_cstr (dir_name (astr_cstr (path)));
+      if (!STREQ (astr_cstr (pdir), "/"))
+        astr_cat_char (pdir, '/');
+      base = astr_new_cstr (base_name (astr_cstr (path)));
     }
   else
     {
-      pdir = s1;
-      base = xstrdup ("");
+      pdir = path;
+      base = astr_new ();
     }
 
-  astr_cpy_cstr (as, base);
-
-  dir = opendir (pdir);
+  DIR *dir = opendir (astr_cstr (pdir));
   if (dir != NULL)
     {
-      astr buf = astr_new ();
-      while ((d = readdir (dir)) != NULL)
+      for (struct dirent *d = readdir (dir); d != NULL; d = readdir (dir))
         {
-          astr_cpy_cstr (buf, pdir);
-          astr_cat_cstr (buf, d->d_name);
-          if (stat (astr_cstr (buf), &st) != -1)
-            {
-              astr_cpy_cstr (buf, d->d_name);
-              if (S_ISDIR (st.st_mode))
-                astr_cat_char (buf, '/');
-            }
-          else
-            astr_cpy_cstr (buf, d->d_name);
+          struct stat st;
+          astr as = astr_new_cstr (d->d_name);
+          if (stat (astr_cstr (astr_cat_cstr (astr_cpy (astr_new (), pdir), d->d_name)), &st) != -1 &&
+              S_ISDIR (st.st_mode))
+            astr_cat_char (as, '/');
           gl_sortedlist_add (cp->completions, completion_strcmp,
-                             xstrdup (astr_cstr (buf)));
+                             xstrdup (astr_cstr (as)));
         }
       closedir (dir);
 
-      cp->path = compact_path (astr_new_cstr (pdir));
+      cp->path = compact_path (pdir);
     }
 
   return dir != NULL;

src/buffer.c
@@ -119,26 +119,6 @@ adjust_markers (size_t o, ptrdiff_t delta)
 }
 
 /*
- * Check the case of a string.
- * Returns 2 if it is all upper case, 1 if just the first letter is,
- * and 0 otherwise.
- */
-static int
-check_case (const char *s, size_t len)
-{
-  size_t i;
-
-  for (i = 0; i < len && isupper ((int) s[i]); i++)
-    ;
-  if (i == len)
-    return 2;
-  else if (i == 1)
-    for (; i < len && !isupper ((int) s[i]); i++)
-      ;
-  return i == len;
-}
-
-/*
  * Insert the character `c' at the current point position
  * into the current buffer.
  * In overwrite mode, overwrite if current character isn't the end of
@@ -189,6 +169,25 @@ delete_char (void)
   goto_offset (o);
 
   return true;
+}
+
+/*
+ * Check the case of a string.
+ * Returns 2 if it is all upper case, 1 if just the first letter is,
+ * and 0 otherwise.
+ */
+static int
+check_case (const char *s, size_t len)
+{
+  size_t i;
+  for (i = 0; i < len && isupper ((int) s[i]); i++)
+    ;
+  if (i == len)
+    return 2;
+  else if (i == 1)
+    for (; i < len && !isupper ((int) s[i]); i++)
+      ;
+  return i == len;
 }
 
 /*
@@ -411,6 +410,12 @@ delete_region (const Region r)
   buffer_replace (cur_bp, r.start, get_region_size (r), NULL, 0, false);
 
   return true;
+}
+
+bool
+in_region (size_t o, size_t x, Region r)
+{
+  return o + x >= r.start && o + x < r.end;
 }
 
 /*

src/extern.h
@@ -71,6 +71,7 @@ int warn_if_readonly_buffer (void);
 Region region_new (size_t o1, size_t o2);
 Region calculate_the_region (void);
 bool delete_region (const Region r);
+bool in_region (size_t o, size_t x, Region r);
 void set_temporary_buffer (Buffer * bp);
 void activate_mark (void);
 void deactivate_mark (void);

src/file.c
@@ -223,7 +223,6 @@ find_file (const char *filename)
 
   Buffer *bp = buffer_new ();
   set_buffer_names (bp, filename);
-
   switch_to_buffer (bp);
 
   estr es = estr_readf (filename);
@@ -435,7 +434,7 @@ END_DEFUN
  * Write buffer to given file name with given mode.
  */
 static int
-raw_write_to_disk (Buffer * bp, const char *filename, mode_t mode)
+write_to_disk (Buffer * bp, const char *filename, mode_t mode)
 {
   int fd = creat (filename, mode);
 
@@ -489,17 +488,17 @@ create_backup_filename (const char *filename, const char *backupdir)
  * Create a backup file if specified by the user variables.
  */
 static int
-write_to_disk (Buffer * bp, const char *filename)
+backup_and_write (Buffer * bp, const char *filename)
 {
-  int fd, backup = get_variable_bool ("make-backup-files"), ret;
-  const char *backupdir = get_variable_bool ("backup-directory") ?
-    get_variable ("backup-directory") : NULL;
-
   /* Make backup of original file. */
+  int fd, backup = get_variable_bool ("make-backup-files");
   if (!get_buffer_backup (bp) && backup
       && (fd = open (filename, O_RDWR, 0)) != -1)
     {
       close (fd);
+
+      const char *backupdir = get_variable_bool ("backup-directory") ?
+        get_variable ("backup-directory") : NULL;
       astr bfilename = create_backup_filename (filename, backupdir);
       if (bfilename)
         {
@@ -514,18 +513,16 @@ write_to_disk (Buffer * bp, const char *filename)
         }
     }
 
-  ret = raw_write_to_disk (bp, filename, S_IRUSR | S_IWUSR |
+  int ret = write_to_disk (bp, filename, S_IRUSR | S_IWUSR |
                            S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
-  if (ret != 0)
-    {
-      if (ret == -1)
-        minibuf_error ("Error writing `%s': %s", filename, strerror (errno));
-      else
-        minibuf_error ("Error writing `%s'", filename);
-      return false;
-    }
+  if (ret == 0)
+    return true;
 
-  return true;
+  if (ret == -1)
+    minibuf_error ("Error writing `%s': %s", filename, strerror (errno));
+  else
+    minibuf_error ("Error writing `%s'", filename);
+  return false;
 }
 
 static le *
@@ -568,7 +565,7 @@ write_buffer (Buffer *bp, bool needname, bool confirm,
       set_buffer_needname (bp, false);
       set_buffer_temporary (bp, false);
       set_buffer_nosave (bp, false);
-      if (write_to_disk (bp, astr_cstr (name)))
+      if (backup_and_write (bp, astr_cstr (name)))
         {
           minibuf_write ("Wrote %s", astr_cstr (name));
           set_buffer_modified (bp, false);
@@ -735,7 +732,7 @@ zile_exit (int doabort)
                              get_buffer_filename_or_name (bp),
                              astr_cstr (astr_recase (astr_new_cstr (PACKAGE), case_upper)));
         fprintf (stderr, "Saving %s...\r\n", astr_cstr (buf));
-        raw_write_to_disk (bp, astr_cstr (buf), S_IRUSR | S_IWUSR);
+        write_to_disk (bp, astr_cstr (buf), S_IRUSR | S_IWUSR);
       }
 
   if (doabort)

src/funcs.c
@@ -54,22 +54,6 @@ Cancel current command.
 }
 END_DEFUN
 
-static void
-print_buf (Buffer * old_bp, Buffer * bp)
-{
-  if (get_buffer_name (bp)[0] == ' ')
-    return;
-
-  bprintf ("%c%c%c %-19s %6u  %-17s",
-           old_bp == bp ? '.' : ' ',
-           get_buffer_readonly (bp) ? '%' : ' ',
-           get_buffer_modified (bp) ? '*' : ' ',
-           get_buffer_name (bp), get_buffer_size (bp), "Fundamental");
-  if (get_buffer_filename (bp) != NULL)
-    bprintf ("%s", astr_cstr (compact_path (astr_new_cstr (get_buffer_filename (bp)))));
-  insert_newline ();
-}
-
 void
 write_temp_buffer (const char *name, bool show, void (*func) (va_list ap), ...)
 {
@@ -137,9 +121,19 @@ write_buffers_list (va_list ap)
   bp = get_window_bp (old_wp);
   do
     {
-      /* Print all buffers except this one (the *Buffer List*). */
-      if (cur_bp != bp)
-        print_buf (get_window_bp (old_wp), bp);
+      /* Print all buffers whose names don't start with space except
+         this one (the *Buffer List*). */
+      if (cur_bp != bp && get_buffer_name (bp)[0] == ' ')
+        {
+          bprintf ("%c%c%c %-19s %6u  %-17s",
+                   get_window_bp (old_wp) == bp ? '.' : ' ',
+                   get_buffer_readonly (bp) ? '%' : ' ',
+                   get_buffer_modified (bp) ? '*' : ' ',
+                   get_buffer_name (bp), get_buffer_size (bp), "Fundamental");
+          if (get_buffer_filename (bp) != NULL)
+            bprintf ("%s", astr_cstr (compact_path (astr_new_cstr (get_buffer_filename (bp)))));
+          insert_newline ();
+        }
       bp = get_buffer_next (bp);
       if (bp == NULL)
         bp = head_bp;

src/line.c
@@ -41,16 +41,10 @@ insert_char (int c)
 }
 
 static void
-insert_expanded_tab (int (*inschr) (int chr))
+insert_expanded_tab (void)
 {
-  undo_start_sequence ();
-
-  int c = get_goalc ();
-  int t = tab_width (cur_bp);
-  for (c = t - c % t; c > 0; --c)
-    (*inschr) (' ');
-
-  undo_end_sequence ();
+  size_t t = tab_width (cur_bp);
+  bprintf ("%*s", t - get_goalc () % t);
 }
 
 static bool
@@ -62,7 +56,7 @@ insert_tab (void)
   if (get_variable_bool ("indent-tabs-mode"))
     insert_char ('\t');
   else
-    insert_expanded_tab (insert_char);
+    insert_expanded_tab ();
 
   return true;
 }
@@ -292,7 +286,7 @@ backward_delete_char_overwrite (void)
 
   backward_char ();
   if (following_char () == '\t')
-    insert_expanded_tab (insert_char);
+    insert_expanded_tab ();
   else
     insert_char (' ');
   backward_char ();

src/term_redisplay.c
@@ -45,12 +45,6 @@ make_char_printable (int c, int x, int cur_tab_width)
     return xasprintf ("\\%o", c & 0xff);
 }
 
-static bool
-in_region (size_t o, size_t x, Region r)
-{
-  return o + x >= r.start && o + x < r.end;
-}
-
 static void
 draw_line (size_t line, size_t startcol, Window * wp,
            size_t o, Region r, int highlight, size_t cur_tab_width)
@@ -94,45 +88,6 @@ calculate_highlight_region (Window * wp, Region * rp)
 
   *rp = region_new (window_o (wp), get_marker_o (get_buffer_mark (get_window_bp (wp))));
   return true;
-}
-
-static void
-draw_window (size_t topline, Window * wp)
-{
-  size_t i, o;
-  Region r;
-  int highlight = calculate_highlight_region (wp, &r);
-
-  /* Find the first line to display on the first screen line. */
-  for (o = estr_start_of_line (get_buffer_text (get_window_bp (wp)), window_o (wp)), i = get_window_topdelta (wp);
-       i > 0 && o > 0;
-       assert ((o = estr_prev_line (get_buffer_text (get_window_bp (wp)), o)) != SIZE_MAX), --i)
-    ;
-
-  /* Draw the window lines. */
-  size_t cur_tab_width = tab_width (get_window_bp (wp));
-  for (i = topline; i < get_window_eheight (wp) + topline; ++i)
-    {
-      /* Clear the line. */
-      term_move (i, 0);
-      term_clrtoeol ();
-
-      /* If at the end of the buffer, don't write any text. */
-      if (o == SIZE_MAX)
-        continue;
-
-      draw_line (i, get_window_start_column (wp), wp, o, r, highlight, cur_tab_width);
-
-      if (get_window_start_column (wp) > 0)
-        {
-          term_move (i, 0);
-          term_addstr("$");
-        }
-
-      o = estr_next_line (get_buffer_text (get_window_bp (wp)), o);
-    }
-
-  set_window_all_displayed (wp, o >= get_buffer_size (get_window_bp (wp)));
 }
 
 static const char *
@@ -202,6 +157,50 @@ draw_status_line (size_t line, Window * wp)
   term_attrset (FONT_NORMAL);
 }
 
+static void
+draw_window (size_t topline, Window * wp)
+{
+  size_t i, o;
+  Region r;
+  int highlight = calculate_highlight_region (wp, &r);
+
+  /* Find the first line to display on the first screen line. */
+  for (o = estr_start_of_line (get_buffer_text (get_window_bp (wp)), window_o (wp)), i = get_window_topdelta (wp);
+       i > 0 && o > 0;
+       assert ((o = estr_prev_line (get_buffer_text (get_window_bp (wp)), o)) != SIZE_MAX), --i)
+    ;
+
+  /* Draw the window lines. */
+  size_t cur_tab_width = tab_width (get_window_bp (wp));
+  for (i = topline; i < get_window_eheight (wp) + topline; ++i)
+    {
+      /* Clear the line. */
+      term_move (i, 0);
+      term_clrtoeol ();
+
+      /* If at the end of the buffer, don't write any text. */
+      if (o == SIZE_MAX)
+        continue;
+
+      draw_line (i, get_window_start_column (wp), wp, o, r, highlight, cur_tab_width);
+
+      if (get_window_start_column (wp) > 0)
+        {
+          term_move (i, 0);
+          term_addstr("$");
+        }
+
+      o = estr_next_line (get_buffer_text (get_window_bp (wp)), o);
+    }
+
+  set_window_all_displayed (wp, o >= get_buffer_size (get_window_bp (wp)));
+
+  /* Draw the status line only if there is available space after the
+     buffer text space. */
+  if (get_window_fheight (wp) - get_window_eheight (wp) > 0)
+    draw_status_line (topline + get_window_eheight (wp), wp);
+}
+
 void
 term_redisplay (void)
 {
@@ -239,11 +238,6 @@ term_redisplay (void)
         cur_topline = topline;
 
       draw_window (topline, wp);
-
-      /* Draw the status line only if there is available space after the
-         buffer text space. */
-      if (get_window_fheight (wp) - get_window_eheight (wp) > 0)
-        draw_status_line (topline + get_window_eheight (wp), wp);
 
       topline += get_window_fheight (wp);
     }

commit dda130eeda6577f710e84f442779814f369a6f23

Fix completion_readdir, broken by commit bb28e0734.

src/completion.c
@@ -204,18 +204,18 @@ completion_readdir (Completion * cp, astr path)
 
   /* Split up path with dirname and basename, unless it ends in `/',
      in which case it's considered to be entirely dirname. */
-  astr pdir, base;
+  astr pdir;
   if (astr_get (path, astr_len (path) - 1) != '/')
     {
       pdir = astr_new_cstr (dir_name (astr_cstr (path)));
       if (!STREQ (astr_cstr (pdir), "/"))
         astr_cat_char (pdir, '/');
-      base = astr_new_cstr (base_name (astr_cstr (path)));
+      astr_cpy_cstr (path, base_name (astr_cstr (path)));
     }
   else
     {
-      pdir = path;
-      base = astr_new ();
+      pdir = astr_cpy (astr_new (), path);
+      astr_truncate (path, 0);
     }
 
   DIR *dir = opendir (astr_cstr (pdir));