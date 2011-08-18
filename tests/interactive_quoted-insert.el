700235656e6b3933065bcef7f00b2fd2dc15d4c8
Gary V. Vaughan <gary@vaughan.pe>
2011-08-18 23:39:17 +0700

    tests: improved interactive_quoted-insert.el (from lua)
    from lua sha 3ef8abe

--- tests/interactive_quoted-insert.el
+++ tests/interactive_quoted-insert.el
@@ -1,2 +1,5 @@
-; quoted-insert C-a save-buffer save-buffers-kill-emacs
-(execute-kbd-macro "\C-q\C-a\C-x\C-s\C-x\C-c")
+; quoted-insert C-a quoted-insert C-h quoted-insert C-z quoted-insert C-q
+; quoted-insert ESC O P quoted-insert TAB save-buffer save-buffers-kill-emacs
+;
+; FIXME: don't allow silly things like "\C-q\F1"
+(execute-kbd-macro "\C-q\C-a\C-q\C-h\C-q\C-z\C-q\C-q\C-q\eOP\C-q\t\C-x\C-s\C-x\C-c")
