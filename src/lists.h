80af194ec85d680ed66533373b42b4f64960dc7b
Reuben Thomas <rrt@sc3d.org>
2012-01-31 21:09:36 +0000

    Update copyright years.

--- src/lists.h
+++ src/lists.h
@@ -1,6 +1,6 @@
 /* Lisp variables
 
-   Copyright (c) 2001, 2005, 2008 Free Software Foundation, Inc.
+   Copyright (c) 2001, 2005, 2008, 2011 Free Software Foundation, Inc.
 
    This file is part of GNU Zile.
 
