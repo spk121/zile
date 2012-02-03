258f32a4fd8231d807bc9db5e4117bf08aee6f1b
Reuben Thomas <rrt@sc3d.org>
2012-02-03 19:56:51 +0000

    configury: update gnulib and autoconf-archive.

--- src/term_curses.c
+++ src/term_curses.c
@@ -22,15 +22,15 @@
 #include <config.h>
 
 #include <stdlib.h>
-#if defined(HAVE_NCURSESW_CURSES_H)
+#if defined HAVE_NCURSESW_CURSES_H
 #  include <ncursesw/curses.h>
-#elif defined(HAVE_NCURSESW_H)
+#elif defined HAVE_NCURSESW_H
 #  include <ncursesw.h>
-#elif defined(HAVE_NCURSES_CURSES_H)
+#elif defined HAVE_NCURSES_CURSES_H
 #  include <ncurses/curses.h>
-#elif defined(HAVE_NCURSES_H)
+#elif defined HAVE_NCURSES_H
 #  include <ncurses.h>
-#elif defined(HAVE_CURSES_H)
+#elif defined HAVE_CURSES_H
 #  include <curses.h>
 #else
 #  error "SysV or X/Open-compatible Curses header file required"
