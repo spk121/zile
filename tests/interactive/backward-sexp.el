be85c7a895f9c424e0a3202003ab575b86fa0cb6
Gary V. Vaughan <gary@gnu.org>
2012-02-19 19:28:11 +0700

    tests: fix tests/interactive/backward-sexp.el for Emacs.
    Emacs behaves strangely in batch mode, adding a paren on the end
    of the second line of a file for (execute-kbd-macro "\M-<\C-e)")
    so put the sexp closing paren at the end of the last line
    instead, which even batch mode does correctly.
    * tests/interactive/backward-sexp.el: Put the closing paren
    before the final newline of the test file.
    * tests/interactive/backward-sexp.output: Adjust.

--- tests/interactive/backward-sexp.el
+++ tests/interactive/backward-sexp.el
@@ -1,2 +1,3 @@
-; ( end-of-line ) backward-sexp delete-char save-buffer save-buffers-kill-emacs
-(execute-kbd-macro "(\C-e)\C-\M-b\C-d\C-x\C-s\C-x\C-c")
+; beginning-of-buffer ( end-of-buffer backward-char ) backward-sexp delete-char
+;  save-buffer save-buffers-kill-emacs
+(execute-kbd-macro "\M-<(\M->\C-b)\C-\M-b\C-d\C-x\C-s\C-x\C-c")
