025df6777d2d2d227a4eaa9df45e5fa622f4b6b3
Gary V. Vaughan <gary@gnu.org>
2012-02-19 22:26:18 +0700

    tests: upcase-region and downcase-region are disabled in GNU Emacs.
    Make these tests zile-only, because we don't have the enable
    disable machinery in zile.
    * tests/interactive/downcase-region.el,
    tests/interactive/downcase-region.output,
    tests/interactive/upcase-region.el,
    tests/interactive/upcase-region.output: Move from here...
    * tests/zile-only/interactive/downcase-region.el,
    tests/zile-only/interactive/downcase-region.output,
    tests/zile-only/interactive/upcase-region.el,
    tests/zile-only/interactive/upcase-region.output: ...to here.

--- tests/interactive/downcase-region.el
+++
@@ -1,3 +0,0 @@
-; set-mark forward-line forward-line downcase-region
-; save-buffer save-buffers-kill-emacs
-(execute-kbd-macro "\C-@\C-n\C-n\C-x\C-l\C-x\C-s\C-x\C-c")
--- tests/interactive/upcase-region.el
+++
@@ -1,3 +0,0 @@
-; set-mark forward-line forward-line upcase-region
-; save-buffer save-buffers-kill-emacs
-(execute-kbd-macro "\C-@\C-n\C-n\C-x\C-u\C-x\C-s\C-x\C-c")
