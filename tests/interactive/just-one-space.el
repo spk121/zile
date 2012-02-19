f6c50e20c948ede8393d0820dd2ca37c8b4e3530
Gary V. Vaughan <gary@gnu.org>
2012-02-19 19:41:55 +0700

    tests: fix tests/interactive/just-one-space.el for Emacs.
    Use Emacs compatible read-syntax for spaces.
    * tests/interactive/just-one-space.el: Use literal spaces
    instead of zile-only \SPC read-syntax for spaces.

--- tests/interactive/just-one-space.el
+++ tests/interactive/just-one-space.el
@@ -1,3 +1,3 @@
 ; forward-word SPC SPC SPC just-one-space insert "a"
 ; save-buffer save-buffers-kill-emacs
-(execute-kbd-macro "\M-f\SPC\SPC\SPC\M-\SPCa\C-x\C-s\C-x\C-c")
+(execute-kbd-macro "\M-f   \M- a\C-x\C-s\C-x\C-c")
