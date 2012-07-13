d06c85892ca4e5539210e60f183760913f8f6507
Reuben Thomas <rrt@sc3d.org>
2012-07-13 11:17:04 +0100

    tests: fix interactive/find-file-read-only for VPATH build

--- tests/interactive/find-file-read-only.el
+++ tests/interactive/find-file-read-only.el
@@ -1,2 +1,2 @@
-; kill-buffer RET find-file-read-only "tests/test.input" save-buffers save-buffers-kill-emacs
-(execute-kbd-macro "\C-xk\r\C-x\C-rtests/test.input\r\C-x\C-s\C-x\C-c")
+; kill-buffer RET find-file-read-only "tests/interactive/find-file-read-only.input" save-buffers save-buffers-kill-emacs
+(execute-kbd-macro "\C-xk\r\C-x\C-rtests/interactive/find-file-read-only.input\r\C-x\C-s\C-x\C-c")
