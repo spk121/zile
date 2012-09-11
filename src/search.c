a2721721d78f61f289a1bce798466b936a6aab97
Reuben Thomas <rrt@sc3d.org>
2012-09-11 21:47:04 +0100

    search.c: fix docstring of isearch-backward-regexp

--- src/search.c
+++ src/search.c
@@ -356,10 +356,10 @@ END_DEFUN
 
 DEFUN ("isearch-backward-regexp", isearch_backward_regexp)
 /*+
-Do incremental search forward for regular expression.
+Do incremental search backward for regular expression.
 With a prefix argument, do a regular string search instead.
 Like ordinary incremental search except that your input
-is treated as a regexp.  See @kbd{M-x isearch-forward} for more info.
+is treated as a regexp.  See @kbd{M-x isearch-backward} for more info.
 +*/
 {
   ok = isearch (false, !(lastflag & FLAG_SET_UNIARG));
